1cbf4f28485024fa1b87a5a3f8e905f126174d42
Kevin Lindemark <KevinLindemark@github.com>
2023-02-19 22:38:18 +0100

    fixed reversed temp hum return values

--- ada_dht.py
+++ ada_dht.py
@@ -19,6 +19,6 @@ def read_sensor():
     # If this happens try again!
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        return humidity, temperature
+        return temperature, humidity
     else:
         print('Failed to get reading. Try again!')
